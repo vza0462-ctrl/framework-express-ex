80b6665dec4dd7d14e36dcf4c1a2c636e6d8116e
vza <vza0462@gmail.com>
2026-05-17 22:56:42 +0700

    first

---
+++ controllers/authorController.js
@@ -0,0 +1,25 @@
+const db = require("./db");
+
+async function getAuthorById(req, res) {
+  const { authorId } = req.params;
+
+  try {
+    const author = await db.getAuthorById(Number(authorId));
+
+    if (!author) {
+      res.status(404).send("Author not found");
+      return;
+    }
+
+    res.send(`Author Name: ${author.name}`);
+  } catch (error) {
+    console.error("Error retrieving author:", error);
+    res.status(500).send("Internal Server Error");
+
+    // or we can call next(error) instead of sending a response here
+    // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
+    // So we will need to create a special type of middleware function if we want a different response and we will get to that... next.
+  }
+}
+
+module.exports = { getAuthorById };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,67 @@
 const { Router } = require("express");
 
 const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/new", text: "New Message" },
+];
+
 indexRouter.get("/", (req, res) => {
-  console.log("ae" + req.customProperty);
-  res.send("index all");
+  res.render("index", {
+    title: "Mini Messageboard",
+    links,
+    messages,
+  });
+});
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form", {
+    title: "New Message",
+    links,
+  });
+});
+
+indexRouter.post("/new", (req, res) => {
+  const { messageUser, messageText } = req.body;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
+indexRouter.get("/messages/:messageId", (req, res) => {
+  const messageId = Number.parseInt(req.params.messageId, 10);
+  const message = messages[messageId];
+
+  if (!message) {
+    res.status(404).send("Message not found");
+    return;
+  }
+
+  res.render("message", {
+    title: "Message Details",
+    links,
+    message,
+    messageId,
+  });
 });
 
 module.exports = indexRouter;
